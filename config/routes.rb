2be218c235cd8e0a698a5ff9ede0b14e5eae5980
Karl Glatzel <karl.glatzel@gmail.com>
2018-11-12 13:05:02 +0100

    add first rails project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
+  # Prefix Verb URI Pattern       Controller#Action
+  #    ask GET  /ask(.:format)    questions#ask
+  # answer GET  /answer(.:format) questions#answer
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
